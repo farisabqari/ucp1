e4c6dd0f068b99d9504fea2f267924ace1e9255a
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-03-29 10:42:38 +0800

    memasukan using namespace

--- ucp1/ucp1.cpp
+++ ucp1/ucp1.cpp
@@ -1,20 +1,34 @@
-// ucp1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+double rerata(double a, double b) {
+    return (a + b) / 2;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+string status(double c) {
+    if (c >= 60)
+        return "lulus";
+    else
+        return "gagal";
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string status2(double r, double n) {
+    if (r >= 60 && n >= 70)
+        return "lulus";
+    else
+        return "gagal";
+}
+
+string status3(double r, double n, double m) {
+    if (r >= 60 || n >= 70 && m >= 60)
+        return "lulus";
+    else
+        return "gagal";
+}
+
+int main() {
+    double nilM, nilB;
+
+    cout << "Masukan Nilai Matematika = ";
+}
